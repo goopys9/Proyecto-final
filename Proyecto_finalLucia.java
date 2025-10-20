5bb549295740d5c5815004792deeeb0075e2c937
Lucia Orobio <sgutierrez@unipacifico.edu.co>
2025-10-20 12:48:47 -0500

    Delete Proyecto_finalLucia.java

--- Proyecto_finalLucia.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.proyecto_finallucia;
-
-public class Proyecto_finalLucia {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
